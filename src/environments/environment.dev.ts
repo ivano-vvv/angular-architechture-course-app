92ff54df92d00272aa5f13176da8025711f68615
Vladislav <ivanovlad2010@gmail.com>
2021-08-18 10:30:18 +0300

    env: firebase config added

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: false,
   name: 'dev',
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyCu2Xafmm_QMWnw1YUzd9HLvgKV-8B9XAo',
+      authDomain: 'angular-architechture-course.firebaseapp.com',
+      projectId: 'angular-architechture-course',
+      storageBucket: 'angular-architechture-course.appspot.com',
+      messagingSenderId: '983941060571',
+      appId: '1:983941060571:web:53ad859c1e911ad140b9fe',
+    },
+  },
 };
