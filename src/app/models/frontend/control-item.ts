45e16be79f6522ed7b6102bf445a04752c0b9e99
Vladislav <ivanovlad2010@gmail.com>
2021-09-27 08:29:07 +0300

    models created

---
+++ src/app/models/frontend/control-item.ts
@@ -0,0 +1,7 @@
+import { Icon } from './icon';
+
+export type ControlItem = {
+  value: primitive;
+  label: string;
+  icon?: Icon;
+};
---
+++ src/app/models/frontend/icon.ts
@@ -0,0 +1,4 @@
+export type Icon = {
+    source: string;
+    cssClass: string;
+}
