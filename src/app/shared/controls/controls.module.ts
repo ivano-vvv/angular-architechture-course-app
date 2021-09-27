01ea99151cbf7f595f0d07a08810a24bd378b725
Vladislav <ivanovlad2010@gmail.com>
2021-09-27 08:07:18 +0300

    shared: password added

--- src/app/shared/controls/controls.module.ts
+++ src/app/shared/controls/controls.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { InputModule } from './input';
 import { FormFieldModule } from './form-field';
+import { PasswordModule } from './password';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, InputModule, FormFieldModule],
-  exports: [InputModule, FormFieldModule],
+  imports: [CommonModule, InputModule, FormFieldModule, PasswordModule],
+  exports: [InputModule, FormFieldModule, PasswordModule],
 })
 export class ControlsModule {}
--- src/app/shared/controls/index.ts
+++ src/app/shared/controls/index.ts
@@ -2,3 +2,4 @@ export * from './controls.module';
 
 export * from './input';
 export * from './form-field';
+export * from './password';
