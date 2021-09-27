d169c54b0e88562d9df387d017adeff74b891154
Vladislav <ivanovlad2010@gmail.com>
2021-09-27 07:53:28 +0300

    shared: form field created

--- src/app/shared/controls/controls.module.ts
+++ src/app/shared/controls/controls.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InputModule } from './input';
+import { FormFieldModule } from './form-field';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, InputModule],
-  exports: [InputModule],
+  imports: [CommonModule, InputModule, FormFieldModule],
+  exports: [InputModule, FormFieldModule],
 })
 export class ControlsModule {}
--- src/app/shared/controls/index.ts
+++ src/app/shared/controls/index.ts
@@ -1,3 +1,4 @@
 export * from './controls.module';
 
 export * from './input';
+export * from './form-field';
