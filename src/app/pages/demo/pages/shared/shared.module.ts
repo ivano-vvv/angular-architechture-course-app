86f7e9fd0e5daaf2b8c47ede9e66a956e8473023
Vladislav <ivanovlad2010@gmail.com>
2021-10-05 06:16:01 +0300

    shared page updated

--- src/app/pages/demo/pages/shared/shared.module.ts
+++ src/app/pages/demo/pages/shared/shared.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { ButtonsModule, ControlsModule } from '@app/shared';
-
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
 
+import { ButtonsModule, ControlsModule, IndicatorsModule } from '@app/shared';
 @NgModule({
-  declarations: [SharedComponent],
-  imports: [CommonModule, ReactiveFormsModule, SharedRoutingModule, ButtonsModule, ControlsModule],
+    declarations: [SharedComponent],
+    imports: [
+        CommonModule,
+        SharedRoutingModule,
+        ReactiveFormsModule,
+        ButtonsModule,
+        ControlsModule,
+        IndicatorsModule
+    ]
 })
-export class SharedModule {}
+export class SharedModule { }
