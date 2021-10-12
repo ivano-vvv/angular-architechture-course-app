e9559b390f627b317e256a01518a8cbd4b0c698a
Vladislav <ivanovlad2010@gmail.com>
2021-10-12 07:09:46 +0300

    models: backend models added

---
+++ src/app/models/backend/user/index.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+import { Employee, Recruiter } from './roles';
+
+export interface User {
+  uid: string;
+  name: string;
+  photoURL: string;
+  email: string;
+  country: string;
+  about?: string;
+  roleId: string;
+  role: Employee | Recruiter;
+  created: firebase.firestore.FieldValue;
+  updated?: firebase.firestore.FieldValue;
+}
