44d8cb95c65e6dbbf6f633bbeca03c9720556c67
Vladislav <ivanovlad2010@gmail.com>
2021-11-02 21:30:23 +0300

    app: store imported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromRoot from './store';
+import * as fromCollections from './store/collections';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'architechture-course-app';
+
+  constructor(private readonly store: Store<fromRoot.State>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(new fromCollections.Read());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,23 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 
-import { MatNativeDateModule, MatDateFormats, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import {
+  MatNativeDateModule,
+  MatDateFormats,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NotificationModule } from './services';
+
+import { reducers, effects } from './store';
 
 import { environment } from '@src/environments/environment';
 import { HeaderComponent } from './components';
@@ -19,15 +30,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const APP_DATE_FORMATS: MatDateFormats = {
   parse: {
-      dateInput: { day: 'numeric', month: 'numeric', year: 'numeric' },
+    dateInput: { day: 'numeric', month: 'numeric', year: 'numeric' },
   },
   display: {
-      dateInput: { day: 'numeric', month: 'short', year: 'numeric' },
-      monthYearLabel: { year: 'numeric', month: 'short' },
-      dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
-      monthYearA11yLabel: { year: 'numeric', month: 'long' }
-  }
+    dateInput: { day: 'numeric', month: 'short', year: 'numeric' },
+    monthYearLabel: { year: 'numeric', month: 'short' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
+  },
 };
+
+const StoreDevtoolsModuleEnv = !environment.production
+  ? StoreDevtoolsModule.instrument({ maxAge: 50 })
+  : [];
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -41,11 +56,20 @@ const APP_DATE_FORMATS: MatDateFormats = {
     AngularFireStorageModule,
     MatNativeDateModule,
     NotificationModule.forRoot(),
+
+    StoreModule.forRoot(reducers, {
+      runtimeChecks: {
+        strictActionImmutability: true,
+        strictStateImmutability: true,
+      },
+    }),
+    EffectsModule.forRoot(effects),
+    StoreDevtoolsModuleEnv,
   ],
-    providers: [
-        { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
-        { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS }
-    ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
