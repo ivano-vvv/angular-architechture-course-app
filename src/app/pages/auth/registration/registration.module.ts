173b2ac9a5c7d507606d2c62b0ea8b291b476ebc
Vladislav <ivanovlad2010@gmail.com>
2021-11-19 07:43:50 +0300

    registration: module imports updated

--- src/app/pages/auth/registration/registration.module.ts
+++ src/app/pages/auth/registration/registration.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {
+  FormFieldModule,
+  InputModule,
+  PasswordModule,
+} from '@app/shared/controls';
+import { SpinnerModule } from '@app/shared/indicators';
+import { ButtonModule } from '@app/shared/buttons';
 
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { RegistrationComponent } from './registration.component';
 
-
 @NgModule({
-  declarations: [
-    RegistrationComponent
-  ],
+  declarations: [RegistrationComponent],
   imports: [
+    ButtonModule,
     CommonModule,
-    RegistrationRoutingModule
-  ]
+    FormFieldModule,
+    InputModule,
+    PasswordModule,
+    ReactiveFormsModule,
+    RegistrationRoutingModule,
+    RouterModule,
+    SpinnerModule,
+  ],
 })
-export class RegistrationModule { }
+export class RegistrationModule {}
