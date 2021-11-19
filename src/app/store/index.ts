370f403d5bcf4de89c4e40b6bfcf38d15a550628
Vladislav <ivanovlad2010@gmail.com>
2021-11-19 06:34:05 +0300

    store: user reducer added

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,13 +1,16 @@
 import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromCollections from './collections';
+import * as fromUser from './user';
 
 export interface State {
   collections: fromCollections.CollectionsState;
+  user: fromUser.UserState;
 }
 
 export const reducers: ActionReducerMap<State> = {
   collections: fromCollections.reducer,
+  user: fromUser.reducer,
 };
 
 export const effects = [fromCollections.CollectionsEffects];
