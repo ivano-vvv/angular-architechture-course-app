94b65d522ec480ae68921f9d35aba754dbd15dcf
Vladislav <ivanovlad2010@gmail.com>
2021-11-22 19:57:19 +0300

    registration: form created

--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+
+import { regex } from '@app/shared';
+import { State } from '@app/store';
+
+import { initPasswordRepeatValidator } from './utils/custom-validators';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private store: Store<State>) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup(
+      {
+        email: new FormControl(null, {
+          validators: [
+            Validators.required,
+            Validators.maxLength(128),
+            Validators.pattern(regex.email),
+          ],
+          updateOn: 'blur',
+        }),
+        password: new FormControl(null, {
+          validators: [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(64),
+            Validators.pattern(regex.password),
+          ],
+          updateOn: 'blur',
+        }),
+        passwordRepeat: new FormControl(null, {
+          validators: [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(64),
+            Validators.pattern(regex.password),
+          ],
+          updateOn: 'blur',
+        }),
+      },
+      {
+        validators: [initPasswordRepeatValidator(this.form)],
+      }
+    );
   }
-
 }
