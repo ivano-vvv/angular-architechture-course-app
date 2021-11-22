4c6137f688f1f236a99b2c7b3277cc280a7d6290
Vladislav <ivanovlad2010@gmail.com>
2021-11-22 18:53:01 +0300

    registration: password repeat validator fixed

--- src/app/pages/auth/registration/utils/custom-validators.ts
+++ src/app/pages/auth/registration/utils/custom-validators.ts
@@ -1,22 +1,25 @@
-import { FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
 
 type PasswordControlsKeys = {
   password: 'password';
   passwordRepeat: 'passwordRepeat';
 };
 
-export const PasswordRepeatValidator = (
-  form: FormGroup,
-  keys?: PasswordControlsKeys
-): Record<string, boolean> | null | never => {
-  const password = form.get(keys?.password || 'password');
-  const passwordRepeat = form.get(keys?.passwordRepeat || 'passwordRepeat');
+export const initPasswordRepeatValidator = (form: FormGroup) => {
+  return (_: AbstractControl): ValidationErrors | null => {
+    const password = form.get('password');
+    const passwordRepeat = form.get('passwordRepeat');
 
-  if (!password || !passwordRepeat) {
-    throw new Error('cannot get password controls');
-  }
+    if (!password || !passwordRepeat) {
+      throw new Error('cannot get password controls');
+    }
 
-  return password.value && password.value !== passwordRepeat.value
-    ? { repeat: true }
-    : null;
+    return password.value && password.value !== passwordRepeat.value
+      ? { repeat: true }
+      : null;
+  };
 };
