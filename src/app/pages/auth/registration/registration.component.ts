66590610f9677572b798b6147ef0d5a502bfd22a
Vladislav <ivanovlad2010@gmail.com>
2021-12-10 07:06:03 +0300

    registration: submit handled

--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -27,41 +27,52 @@ export class RegistrationComponent implements OnInit {
   ngOnInit(): void {
     this.loading$ = this.store.pipe(select(fromUser.getLoading));
 
-    this.form = new FormGroup(
-      {
-        email: new FormControl(null, {
-          validators: [
-            Validators.required,
-            Validators.maxLength(128),
-            Validators.pattern(regex.email),
-          ],
-          updateOn: 'blur',
-        }),
-        password: new FormControl(null, {
-          validators: [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(64),
-            Validators.pattern(regex.password),
-          ],
-          updateOn: 'blur',
-        }),
-        passwordRepeat: new FormControl(null, {
-          validators: [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(64),
-            Validators.pattern(regex.password),
-          ],
-          updateOn: 'blur',
-        }),
-      });
+    this.form = new FormGroup({
+      email: new FormControl(null, {
+        validators: [
+          Validators.required,
+          Validators.maxLength(128),
+          Validators.pattern(regex.email),
+        ],
+        updateOn: 'blur',
+      }),
+      password: new FormControl(null, {
+        validators: [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(64),
+          Validators.pattern(regex.password),
+        ],
+        updateOn: 'blur',
+      }),
+      passwordRepeat: new FormControl(null, {
+        validators: [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(64),
+          Validators.pattern(regex.password),
+        ],
+        updateOn: 'blur',
+      }),
+    });
 
     this.form.addValidators([initPasswordRepeatValidator(this.form)]);
   }
 
   handleSubmit(): void {
-    alert('submit');
-    console.log(this.form.value);
+    if (this.form.valid) {
+      this.signUp();
+    } else {
+      this.form.markAsTouched();
+    }
+  }
+
+  private signUp(): void {
+    const credentials: fromUser.EmailPasswordCredentials = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    };
+
+    this.store.dispatch(new fromUser.SignUp(credentials));
   }
 }
